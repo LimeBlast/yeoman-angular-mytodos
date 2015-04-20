2de3b17f5188e4eb69f8fda4afe1281b6e5e3dbe
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-04-20 16:31:33 +0100

    added some unit tests

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.todos = [];
 
     $scope.addTodo = function () {
       console.log($scope.todos.indexOf($scope.todo) == -1);
